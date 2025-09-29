2b818df3043a269db390c1375d785190b10eecda
Maarten Behn <maarten.behn@gmail.com>
2025-09-29 21:46:11 +0200

    stuff

--- flake.nix
+++ flake.nix
@@ -1,3 +1,7 @@
+# Needed to get to work 
+# Downgrade lockfile
+# npm i --lockfile-version 2 --package-lock-only
+
 {
   description = "manage-my-damn-life";
 
@@ -8,9 +12,9 @@
 
   outputs =
     {
-      nixpkgs,
-      flake-parts,
-      ...
+    nixpkgs,
+    flake-parts,
+    ...
     }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
@@ -27,13 +31,80 @@
         ...
         }:
         let
-          manage-my-damn-life = import ./nix {
-            inherit pkgs;
-            inherit system; 
+        name = "manage-my-damn-life";
+        version = "0.8.1"; 
+        
+        build = pkgs.buildNpmPackage rec {
+          pname = "${name}-build";
+          inherit version;
+          src = ./.;
+
+          # Generate a new hash using:
+          # nix develop
+          # npm i --package-lock-only
+          # prefetch-npm-deps package-lock.json
+          npmDepsHash = "sha256-dALbxhzjm2IZva5PdgOYVMKUkvS2QibjoWOq1AWSJZ4=";
+
+          nativeBuildInputs = with pkgs; [ cacert wget ];
+          npmBuildScript = "build";
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir $out
+            cp -rv config $out/config
+            cp -rv models $out/models
+            cp -rv migrations $out/migrations
+            cp -rv public $out/public
+            cp -rv ./.next/standalone/* $out
+            cp -rv ./.next/standalone/.next $out/.next
+            cp -rv ./.next/static $out/.next/static
+            cp -rv node_modules/dotenv $out/node_modules/dotenv 
+
+            runHook postInstall
+            '';
+          #meta.mainProgram = "${pname}";
+
+          # Add internet access to build phase by adding output hash
+            #outputHashAlgo = "sha256";
+            #outputHashMode = "recursive";
+            #outputHash = "sha256-dALbxhzjm2IZva5PdgOYVMKUkvS2QibjoWOq1AWSJZ4=";
+          #outputHash = pkgs.lib.fakeHash;
+        };
+
+        program = pkgs.stdenv.mkDerivation rec {
+          pname = "${name}";
+          inherit version;
+          src = build;
+
+          installPhase = ''
+            mkdir -p $out/lib $out/bin
+            cp -rv . $out/lib 
+ 
+            cat > $out/bin/${pname} << EOF
+            !/bin/sh
+            mkdir -p /var/lib/${pname}
+            cd /var/lib/${pname} 
+            rm -rf lib || true
+            cp -r $out/lib .
+
+            cd lib
+            export NODE_ENV=production
+            export NEXT_PUBLIC_SECRET=mmdl
+            ${pkgs.lib.getExe pkgs.nodejs} server.js
+            EOF
+
+            chmod +x $out/bin/${pname}
+            '';
+        };
+      in {
+          packages.default = program;
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs
+              pkgs.prefetch-npm-deps # see server.nix
+            ];
           };
-        in {
-          packages.default = manage-my-damn-life.package;
-          devshells.default = manage-my-damn-life.shell;
         };
     };
 }
